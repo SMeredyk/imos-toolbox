ed91ecb6861d82be268d4e8f0660d8071815ea9a
Rugged\smeredyk <shawn.meredyk@gmail.com>
2017-03-06 12:24:05 -0500

    Small parser changes from friday to the nitrate data parser (ISUS). Parser is still in development on the side, not a priority.

--- Parser/readISUS.m
+++ Parser/readISUS.m
@@ -1,11 +1,11 @@
-function sample_data = readISUS( foldername, mode )
+function sample_data = readISUS(filename, mode )
 %readISUS Parses several data files retrieved from a Satlantic nitrate sensor ISUS V2 /V3 
 %
 % This function is able to read a folder of .dat files retrieved from an ISUS unit.
-% The data is recovered via the ISUSCom 2.1.6 software
+% The data is recovered via the ISUSCom 2.1.6 software and converted into a single CSV file.
 %
 % Inputs:
-%   foldername  - Cell array containing the name of the folder to parse.
+%   filename  	- Cell array containing the name of the file to parse.
 %   mode        - Toolbox data type mode ('profile' or 'timeSeries').
 %
 % Outputs:
@@ -50,35 +50,23 @@ function sample_data = readISUS( foldername, mode )
 % ensure that there is one or two arguments
 narginchk(1,2);
 
-if ~ischar(foldername), error('foldername must contain a string'); end
+if ~ischar(filename), error('filename must contain a string'); end
+
+[~, serNum, ext] = fileparts(filename);
+   
+data = readData(filename);
+    
  
-
- 
-%read folder contents : directory, filepattern, filterout, fpattern
-%directory = foldername;
-filepattern = 'DAT';
-filterout = 0;
-fpattern = 'DIVE';
-
-[n, datafiles] = get_folder_content(foldername, filepattern, filterout, fpattern);
-
-%now n is number of file names listed in structure files
-% to access file names use this:
-
-%datafiles(1).name % this will give you just the file name of the first file in the list
-%datafiles(1).fullname %this will give you absolute file name ... so with absolute path
- 
-%read the various DAT files to create a consolidated data structure 
-data = readData(n, datafiles);  % read Data and convert
-
 % copy all of the information over to the sample data struct
 sample_data = struct;
 
-sample_data.toolbox_input_file              = datafiles(1).fullname;
+sample_data.toolbox_input_file              = filename;
 sample_data.meta.instrument_make            = 'Satlantic';
 sample_data.meta.instrument_model           = 'V3';
-sample_data.meta.instrument_serial_no       = '135'; % need to extract serial number from main folder name
-sample_data.meta.instrument_sample_interval = median(diff(data.TIME.values*24*3600));
+sample_data.meta.instrument_serial_no       = serNum; 
+% sample_data.meta.instrument_sample_interval =
+% median(diff(data.TIME.values*24*3600)); causes an error because data is
+% not parsed correctly yet.
 sample_data.meta.featureType                = mode;
 
 sample_data.dimensions = {};
@@ -123,143 +111,55 @@ for k = 1:length(fields)
 end
 end %end of main function
 
-function data = readData(n, datafiles)
-%READDATA Reads the sample data from the numerous files in the specified folder.
 
-  data = struct;
+function data = readData(filename)
+%READDATA Reads the sample data from the file.
+
+data = struct;
+  dataDelim = ',';	% comma delimited data 
   
-  dataDelim = ',';
-  dataFmt = '%*s %s %f %f %*[^\n]'; % from pascal`s code
-  metahdr = 'SATNHR';
+  fid = fopen(filename, 'rt');
+  params = textscan(fid, '%s', 1, 'HeaderLines', 1, 'Delimiter', ',');   
+  params = params{1};
+  iParams = strfind(params, ',');
+  nParams = length(iParams{1});
+  paramsFmt = repmat('%s', 1, nParams);
+  params = textscan(params{1}, paramsFmt, 'Delimiter', dataDelim);
+  dataFmt = ['%s', repmat('%f', 1, nParams-1)];
+  values = textscan(fid, dataFmt, 'Delimiter', dataDelim);
+  fclose(fid);
   
-  for p=1:n
-  
-    % Read the data
-	fid = fopen( datafiles(p).name, 'rt' );
-    values = textscan( fid, dataFmt, 'delimiter', dataDelim, 'commentStyle',metahdr ); % modified from pascal`s code
-    fclose( fid );
-
-	% Building Date and Time from DAT file
-	date = values{ 1 };
-	date = char( date );
-	doy = str2num( date(:, 5: end) );
-	date = str2num( date(:, 1: 4) );
-	date = datestr( doy2date( doy, date ) );
-
-	%hours and minutes
-	hours = values{ 2 };
-	hh = floor( hours );
-	minu = rem(hours,hh) * 60;
-
-	% ss = ones( size( minu ) );
-	I =( floor( minu ) == 0 );
-	ss( I ) = minu( I ) * 60;
-	ss( ~I ) = rem( minu(~I), floor( minu(~I) ) ) * 60;
-	tmp = datevec( date );
-	tmp(:,4) = hh;
-	tmp(:,5) = floor(minu);
-	tmp(:,6) = ss;
-	tmp( isnan( tmp ) ) = 0;
-	date = datestr( datenum( tmp ), 'dd-mmm-yyyy HH:MM:SS.FFF' );
-
-	% importing nitrate data into values struct
-	nitrates = values{3};
-	
-	  % Remove values == 0;
-		date( nitrates == 0, : ) = [];
-		nitrates( nitrates == 0 ) = [];
-		
-	%re-import date and nitrates to value struct after zeros removed
-	values{1} = date;
-	values{3} = nitrates;
-	
-    % convert the raw data into real values      
-    data.TIME.values = datenum(values{1});
-    data.TIME.comment = 'Time'; 
-     
-    data.NTRA.values = values{3};
-    data.NTRA.comment = ['mole_concentration_of_nitrate_in_sea_water : [mole l-1]']; 
-	
-  end            
-
-   			  
+% other variables exported by SatCon
+%   T_SPEC	T_LAMP	LAMP_TIME	HUMIDITY	VOLT_12	VOLT_5	REF_AVG	REF_STD	SW_DARK	SPEC_AVG	UV(189.17)
+  for i=1:nParams
+      switch params{i}{1}
+                  
+				  %Date : Still needs testing
+                  case 'DATEFIELD', 
+                    name = 'DATE';																
+                    data.DATE.values = datenum(values{i}, 'yyyy'-'juliandate');
+					data.DATE.comment = ['yyyy-DDD'];
+					
+				%Time (HH:MM:SS.mmm)
+                  case 'TIMEFIELD'
+                    name = 'TIME';
+                    data.TIME.values = datenum(values{i}, 'HH:MM:SS.mmm');
+                    data.TIME.comment = ['HH:MM:SS.mmm'];    
+										  
+				  case 'NITRATE	RMSe', 
+				    name = 'NTRA';
+                    data.NTRA.values = values{i};
+					data.NTRA.comment = ['mole_concentration_of_nitrate_in_sea_water : [mole l-1]']';
+				
+				case 'T_INT', 
+				    name = 'TEMP';
+                    data.TEMP.values = values{i};
+					data.TEMP.comment = ['internal temp of nitrate sensor']';	
+                
+				case 'VOLT_MAIN', 
+				    name = 'VOLT';
+                    data.VOLT.values = values{i};
+					data.VOLT.comment = ['Voltage main of nitrate sensor']';              
+      end % end of Switch
+  end % end of For loop
 end % end of readData function
-
-
-% Function return the folder content filtered
-% function [ n, datafiles ] = get_folder_content( directory, filepattern, filterout, fpattern)
-% 
-% input:
-%        - directory
-%        - filepattern 
-%        - filterout    |  1 or 0
-%        - fpattern     |  not include files with this pattern
-%          fpattern is charracter array created by fpattern =
-%          char('string1', 'string2', ...,'stringn');
-%          or in case of one string fpattern = 'string';
-%        
-% output:
-%        - n | number of files
-%        - datafiles | structure with datafiles
-%          datafiles(i).name -- sigle file name
-%          datafiles(i).fullname -- including full path 
-
-function [ n, datafiles ] = get_folder_content( foldername, filepattern, filterout, fpattern)
-
-%initialisation of the structure
-datafiles(1).name = '';
-datafiles(1).fullname = '';
-  
-  if nargin <3,
-     filterout = 0; 
-  end
-  
-     tmpdir =  dir([foldername filepattern '*']);
-     nn = length(tmpdir);
-     %filtering not necessary files
-     cnt = 0;
-  display('Folder contain files for further processing:')   
-  if filterout==1
-     [nstr s] = size(fpattern);
-     filter_flag = 0;
-     for i=1:nn
-         for st=1:nstr
-            S = regexp(tmpdir(i).name,fpattern(st,:));
-            if(isempty(S))
-                filter_flag=0;
-            else
-                filter_flag=1;
-            end
-         end
-         if(filter_flag==0)
-             % filter pattern not maching ... so this is correct file
-             cnt = cnt+1;
-             datafiles(cnt).name = tmpdir(i).name;
-             datafiles(cnt).fullname = [foldername tmpdir(i).name];
-             
-         else
-             %filter pattern matching 
-             %display(['Filtering out: ' tmpdir(i).name ' file'])
-         end
-     end
-     
-     
-  else
-     for i=1:nn
-         datafiles(i).name = tmpdir(i).name;
-         datafiles(i).fullname = [foldername tmpdir(i).name];
-     end
-      
-  end
-
-  
-  n = length(datafiles);
-  for i=1:n
-      display([ num2str(i) ' file: ' datafiles(i).name ])
-  end
-  display(['Number of listed files: ' num2str(n)]);
-  return
-
-end  
-
-
