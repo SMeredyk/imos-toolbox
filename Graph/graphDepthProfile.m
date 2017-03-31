5494964eec8052f43c5d73ab71203d80ff558184
Guillaume Galibert <guillaume.galibert@utas.edu.au>
2017-03-31 14:20:13 +1100

    Graphing in DepthProfile mode initially zoom on good data only.

--- Graph/graphDepthProfile.m
+++ Graph/graphDepthProfile.m
@@ -1,4 +1,4 @@
-function [graphs lines vars] = graphDepthProfile( parent, sample_data, vars )
+function [graphs, lines, vars] = graphDepthProfile( parent, sample_data, vars )
 %GRAPHDEPTHPROFILE Graphs the given data in a depth profile style using 
 % subplots.
 %
@@ -143,8 +143,8 @@ function [graphs lines vars] = graphDepthProfile( parent, sample_data, vars )
     col = col(mod(k, length(col))+1, :);
     
     % plot the variable
-    plotFunc            = getGraphFunc('DepthProfile', 'graph', name);
-    [lines(k,:) labels] = plotFunc(   graphs(k), sample_data, vars(k));
+    plotFunc             = getGraphFunc('DepthProfile', 'graph', name);
+    [lines(k,:), labels] = plotFunc(graphs(k), sample_data, vars(k));
     
     % set the line colour - wrap in a try block, 
     % as surface plot colour cannot be set
@@ -173,11 +173,11 @@ function [graphs lines vars] = graphDepthProfile( parent, sample_data, vars )
     if sample_data.meta.level == 1 && strcmp(func2str(plotFunc), 'graphDepthProfileGeneric')
         qcSet     = str2double(readProperty('toolbox.qc_set'));
         
-        % set x and y limits so that axis are optimised for data below surface only
+        % set x and y limits so that axis are optimised for good data only
         curData = sample_data.variables{vars(k)}.data;
         curDepth = depth.data;
         curFlag = sample_data.variables{vars(k)}.flags;
-        iGood = curDepth>=0;
+        iGood = (curFlag == 0) | (curFlag == 1) | (curFlag == 2);
 
         yLimits = [floor(min(curDepth(iGood))), ceil(max(curDepth(iGood)))];
         xLimits = [floor(min(curData(iGood))),  ceil(max(curData(iGood)))];
@@ -209,34 +209,4 @@ function [graphs lines vars] = graphDepthProfile( parent, sample_data, vars )
     set(graphs(k), 'YTick', yTicks);
     
   end
-  
-    % GLT : Eventually I prefered not displaying the QC legend as it
-    % influences too badly the quality of the plots. I didn't manage to have
-    % a satisfying result with a ghost axis hosting the legend... So for now
-    % I added the possiblity to the user to right-click on a QC'd data point
-    % and it displays the description of the color flag.
-    % compile variable names for the legend
-%   names = {};
-%   for k = 1:length(vars)
-%     
-%     names{k} = sample_data.variables{vars(k)}.name;
-%   end
-%   
-%   % link axes for panning/zooming, and add a legend - matlab has a habit of
-%   % throwing 'Invalid handle object' errors for no apparent reason (i think 
-%   % when the user changes selections too quickly, matlab is too slow, and 
-%   % ends up confusing itself), so absorb any errors which are thrown
-%   try
-%     linkaxes(graphs, 'y');
-%     
-%     % When adding a single legend for multiple subplots, by default the legend 
-%     % is added to the axis which corresponds to the first handle in the vector 
-%     % that is passed in ('lines' in this case). This is a problem in our case, 
-%     % because it means that the legend will be added to the left most axis, 
-%     % whereas we want it to be added to the right-most axis. To get around 
-%     % this, I'm reversing the order of the line handles (and names) before 
-%     % passing them to the legend function.
-%     legend(flipud(lines(:,1)), fliplr(names));
-%   catch e
-%   end
 end
